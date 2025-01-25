c6097b0ba69d580dbc683a25aab3a4e78ae5a9bc
vladinSon <vladinson009@icloud.com>
2025-01-25 21:30:12 +0100

    custom create select options for form

---
+++ movie-magic-workshop/src/utils/createSelectOptions.js
@@ -0,0 +1,11 @@
+// Create dynamic select options for game platforms
+export default function createSelectOptions(object, selected) {
+  const tuples = Object.entries(object);
+  return tuples.map(([value, content]) => {
+    if (value == selected) {
+      return { value, content, selected: true };
+    } else {
+      return { value, content, selected: false };
+    }
+  });
+}
