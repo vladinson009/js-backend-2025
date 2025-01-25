cd7fc0477e3afeaf4a465ac5e49a45f8fdb94e6c
vladinSon <vladinson009@icloud.com>
2025-01-25 20:36:45 +0100

    added cast route

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import staticController from './controllers/staticController.js';
 import movieController from './controllers/movieController.js';
+import castController from './controllers/castController.js';
 const routes = Router();
 
 routes.use(homeController); // * Home page
 routes.use(staticController); // * About page
 routes.use('/movies', movieController); // * Movie pages
+routes.use('/cast', castController); // * Cast pages
 
 routes.use('*', (req, res) => res.redirect('/404'));
 export default routes;
