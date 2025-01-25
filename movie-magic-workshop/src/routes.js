9ac173f89f64daba9be7466dc366af5c781d4b47
vladinSon <vladinson009@icloud.com>
2025-01-25 19:01:55 +0100

    added staticController & redirect page

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
-import aboutController from './controllers/aboutController.js';
+import staticController from './controllers/staticController.js';
 import movieController from './controllers/movieController.js';
 const routes = Router();
 
 routes.use(homeController); // * Home page
-routes.use(aboutController); // * About page
+routes.use(staticController); // * About page
 routes.use('/movies', movieController); // * Movie pages
 
-routes.use('*', (req, res) => res.render('404'));
+routes.use('*', (req, res) => res.redirect('/404'));
 export default routes;
