2915c9612f1dd65ec29e0dcc1990e46d57b5591e
vladinSon <vladinson009@icloud.com>
2025-01-25 16:37:13 +0100

    added path from constants

--- movie-magic-workshop/src/configs/mongooseConfig.js
+++ movie-magic-workshop/src/configs/mongooseConfig.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-
+import { DB_URL } from '../constants.js';
 export default async function () {
   try {
-    await mongoose.connect('mongodb://localhost:27017/nameSomeDB');
+    await mongoose.connect(DB_URL);
     console.log('connected to DB...');
   } catch (error) {
     console.log('Can not connect to DB...');
