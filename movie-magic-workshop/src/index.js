d4242b4fc620ce9847151b643c68875334c44b74
vladinSon <vladinson009@icloud.com>
2025-01-25 15:52:00 +0100

    mongooseConfigs in index app

--- movie-magic-workshop/src/index.js
+++ movie-magic-workshop/src/index.js
@@ -2,8 +2,12 @@ import express from 'express';
 import 'dotenv/config.js';
 import hbsConfig from './configs/hbsConfig.js';
 import { PORT } from './constants.js';
+import mongooseConfig from './configs/mongooseConfig.js';
+import expressConfig from './configs/expressConfig.js';
 const app = express();
 
 hbsConfig(app);
+expressConfig(app);
+mongooseConfig();
 
 app.listen(PORT, () => console.log(`Server is listneing on http://localhost:${PORT}...`));
