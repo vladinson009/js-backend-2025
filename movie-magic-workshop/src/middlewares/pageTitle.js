8e56cbceb11c0028a1708e51b0e13aa3e9135174
vladinSon <vladinson009@icloud.com>
2025-01-25 20:37:22 +0100

    added cast page title

--- movie-magic-workshop/src/middlewares/pageTitle.js
+++ movie-magic-workshop/src/middlewares/pageTitle.js
@@ -2,9 +2,11 @@ const titles = {
   '/': 'Catalog Page',
   '/404': '404 Page',
   '/about': 'About Page',
-  '/movies/create': 'Create Page',
-  '/movies/details': 'Details Page',
-  '/search': 'Search Page',
+  '/movies/create': 'Create Movie',
+  '/movies/details': 'Movie Details',
+  '/movies/search': 'Search Page',
+  '/cast/attach': 'Attach Cast Page',
+  '/cast/create': 'Create Cast Page',
 };
 
 export default function pageTitle(req, res, next) {
