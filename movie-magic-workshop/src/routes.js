702c483e9bbb92388d5afd6e666c06a1714ea244
vladinSon <vladinson009@icloud.com>
2025-01-25 16:37:01 +0100

    added about controller

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
+import aboutController from './controllers/aboutController.js';
 const routes = Router();
 
-routes.use(homeController);
+routes.use(homeController); // * Home page;
+routes.use(aboutController); // * About page;
+
 routes.use('*', (req, res) => res.render('404'));
 export default routes;
