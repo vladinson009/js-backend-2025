9863a4adb5abac76ccd74f64450bcafd8c9de8c2
vladinSon <vladinson009@icloud.com>
2025-01-25 15:33:23 +0100

    started app

--- movie-magic-workshop/src/index.js
+++ movie-magic-workshop/src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import 'dotenv/config.js';
 import hbsConfig from './configs/hbsConfig.js';
-
+import { PORT } from './constants.js';
 const app = express();
 
 hbsConfig(app);
+
+app.listen(PORT, () => console.log(`Server is listneing on http://localhost:${PORT}...`));
