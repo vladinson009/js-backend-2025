ab29be1114c870e54ba720921451d3dadba2c15f
vladinSon <vladinson009@icloud.com>
2025-01-25 19:02:45 +0100

    added dynamic movies from db

--- movie-magic-workshop/src/controllers/homeController.js
+++ movie-magic-workshop/src/controllers/homeController.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import movieServices from '../services/movieServices.js';
 
 const homeController = Router();
 
-homeController.get('/', (req, res) => {
-  res.render('home');
+homeController.get('/', async (req, res) => {
+  try {
+    const movies = await movieServices.getAll().lean();
+    res.render('home', { movies });
+  } catch (error) {
+    res.render('home', { movies: [] });
+  }
 });
 
 export default homeController;
