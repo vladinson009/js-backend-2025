e934ee22c8f1a173cc2c9f162c6babe6700de669
vladinSon <vladinson009@icloud.com>
2025-01-25 20:37:42 +0100

    added new validation to Movie schema

--- movie-magic-workshop/src/models/Movie.js
+++ movie-magic-workshop/src/models/Movie.js
@@ -1,5 +1,5 @@
-import { Schema, model } from 'mongoose';
-
+import { Schema, model, Types } from 'mongoose';
+const currentYear = new Date().getFullYear();
 const movieSchema = new Schema({
   title: {
     type: String,
@@ -20,19 +20,38 @@ const movieSchema = new Schema({
   year: {
     type: Number,
     required: true,
+    max: currentYear,
+    min: 1950,
   },
   imageUrl: {
     type: String,
     required: true,
+    validate: {
+      validator(el) {
+        const pattern = /^https?:\/\//;
+        return pattern.test(el);
+      },
+      message: 'Not a valid URL',
+    },
   },
   rating: {
     type: Number,
     required: true,
+    max: 10,
+    min: 1,
   },
   description: {
     type: String,
     required: true,
+    maxLength: 150,
   },
+  casts: [
+    {
+      _id: false,
+      nameInMovie: String,
+      casts: { type: Types.ObjectId, ref: 'cast' },
+    },
+  ],
 });
 
 export default model('movie', movieSchema);
