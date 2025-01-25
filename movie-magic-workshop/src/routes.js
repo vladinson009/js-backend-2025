4decf71baf10ccabc2526c5520d1e48ed711c035
vladinSon <vladinson009@icloud.com>
2025-01-25 17:00:18 +0100

    added movies controller

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import aboutController from './controllers/aboutController.js';
+import movieController from './controllers/movieController.js';
 const routes = Router();
 
-routes.use(homeController); // * Home page;
-routes.use(aboutController); // * About page;
+routes.use(homeController); // * Home page
+routes.use(aboutController); // * About page
+routes.use('/movies', movieController); // * Movie pages
 
 routes.use('*', (req, res) => res.render('404'));
 export default routes;
