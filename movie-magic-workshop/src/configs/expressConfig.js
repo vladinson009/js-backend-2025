0a0b16ce4bcb68ebbfdb48c43498e3ab2e44aff2
vladinSon <vladinson009@icloud.com>
2025-01-25 19:02:18 +0100

    form data parser & dynamic page title

--- movie-magic-workshop/src/configs/expressConfig.js
+++ movie-magic-workshop/src/configs/expressConfig.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import routes from '../routes.js';
+import pageTitle from '../middlewares/pageTittle.js';
 export default function (app) {
   app.use(express.static('public'));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(pageTitle);
   app.use(routes);
 }
