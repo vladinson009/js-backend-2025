6f29675bdd375c705c6f454cacd18a9970c8f4ba
vladinSon <vladinson009@icloud.com>
2025-01-25 19:39:57 +0100

    get by criteria service

--- movie-magic-workshop/src/services/movieServices.js
+++ movie-magic-workshop/src/services/movieServices.js
@@ -12,5 +12,23 @@ function createMovie(formData) {
 
   return Movie.create(formData);
 }
+function getByCriteria(criteria) {
+  let query = Movie.find();
+  if (criteria.title) {
+    query = query.find({ title: { $regex: new RegExp(criteria.title, 'i') } });
+  }
+  if (criteria.genre) {
+    query = query.find({ genre: { $regex: new RegExp(criteria.genre, 'i') } });
+  }
+  if (criteria.year) {
+    query = query.find({
+      year: {
+        $gte: Number(criteria.year) - 1,
+        $lte: Number(criteria.year) + 1,
+      },
+    });
+  }
+  return query;
+}
 
-export default { getAll, createMovie };
+export default { getAll, createMovie, getByCriteria };
