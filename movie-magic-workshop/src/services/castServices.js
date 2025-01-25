0142ba9cafedc798208899cc8d18fbb966c45c9f
vladinSon <vladinson009@icloud.com>
2025-01-25 23:58:18 +0100

    implemented add movie to cast's collection

--- movie-magic-workshop/src/services/castServices.js
+++ movie-magic-workshop/src/services/castServices.js
@@ -1,7 +1,25 @@
 import Cast from '../models/Cast.js';
-
+import mongoose from 'mongoose';
 function create(data) {
   return Cast.create(data);
 }
+function addMovie(castId, movieId) {
+  return Cast.findByIdAndUpdate(
+    castId,
+    {
+      $push: {
+        movies: movieId,
+      },
+    },
+    { runValidators: true, new: true }
+  );
+}
+function fetchCast(ninMovieId) {
+  return Cast.find({
+    movies: {
+      $nin: [ninMovieId],
+    },
+  });
+}
 
-export default { create };
+export default { create, fetchCast, addMovie };
