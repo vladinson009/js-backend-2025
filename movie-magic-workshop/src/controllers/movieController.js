f88785a82c80c678cc48be90d80a6082d1c539dc
vladinSon <vladinson009@icloud.com>
2025-01-25 20:37:11 +0100

    implement get movie by id service

--- movie-magic-workshop/src/controllers/movieController.js
+++ movie-magic-workshop/src/controllers/movieController.js
@@ -17,15 +17,24 @@ movieController.post('/create', async (req, res) => {
   }
 });
 // ! movie detais get request
-movieController.get('/details/:movieId', (req, res) => {
-  res.render('movie/details');
+movieController.get('/details/:movieId', async (req, res) => {
+  try {
+    const { movieId } = req.params;
+    const movie = await movieServices.getById(movieId).lean();
+    movie.rating = '★'.repeat(Math.floor(movie.rating / 2));
+    res.render('movie/details', { movie });
+  } catch (error) {
+    res.redirect('/404');
+  }
 });
+// ! movie search get request
 movieController.get('/search', async (req, res) => {
   try {
     const movies = await movieServices.getAll().lean();
     res.render('movie/search', { movies });
   } catch (error) {}
 });
+// ! movie search with criteria get request
 movieController.get('/searchBy', async (req, res) => {
   try {
     const filter = req.query;
