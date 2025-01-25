e63913b6e113d7db76e1d8b0db3155c7cf78418c
vladinSon <vladinson009@icloud.com>
2025-01-25 16:04:52 +0100

    added 404 route

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -3,5 +3,5 @@ import homeController from './controllers/homeController.js';
 const routes = Router();
 
 routes.use(homeController);
-
+routes.use('*', (req, res) => res.render('404'));
 export default routes;
