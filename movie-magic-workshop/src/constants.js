f4a43b4e91333853b093fbb4fb266fd58380a361
vladinSon <vladinson009@icloud.com>
2025-01-26 14:55:04 +0100

    added JWT secret word constant

--- movie-magic-workshop/src/constants.js
+++ movie-magic-workshop/src/constants.js
@@ -1,2 +1,3 @@
 export const PORT = process.env.PORT || 2000;
 export const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/';
+export const JWT_SECRET_WORD = process.env.JWT_SECRET_WORD || null;
