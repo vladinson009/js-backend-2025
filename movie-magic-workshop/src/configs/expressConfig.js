2ed7bea3d7b109a2c4959487857d552a897504ec
vladinSon <vladinson009@icloud.com>
2025-01-26 15:00:39 +0100

    added cookie parser middleware & comments

--- movie-magic-workshop/src/configs/expressConfig.js
+++ movie-magic-workshop/src/configs/expressConfig.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from '../routes.js';
 import pageTitle from '../middlewares/pageTitle.js';
+import cookieParser from 'cookie-parser';
+// * middlewares for following functionality
 export default function (app) {
-  app.use(express.static('public'));
-  app.use(express.urlencoded({ extended: false }));
-  app.use(pageTitle);
-  app.use(routes);
+  app.use(express.static('public')); // ! load static files
+  app.use(express.urlencoded({ extended: false })); // ! req.body(POST) || req.query(GET)
+  app.use(cookieParser()); // ! req.cookies
+  app.use(pageTitle); // ! dynamic changes of page title
+  app.use(routes); // ! main router for url
 }
