d05b22247df40cff00572814dcffa99b618c1438
vladinSon <vladinson009@icloud.com>
2025-01-26 16:13:13 +0100

    implemented user controller for register & logout & get req for login

---
+++ movie-magic-workshop/src/controllers/userController.js
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import userServices from '../services/userServices.js';
+import errorParser from '../utils/customErrorHandler.js';
+import createToken from '../utils/createJwt.js';
+
+const userController = Router();
+
+// Login Form
+userController.get('/login', (req, res) => {
+  res.render('user/login');
+});
+
+// Register Form
+userController.get('/register', (req, res) => {
+  res.render('user/register');
+});
+userController.post('/register', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const user = await userServices.register(userInput);
+    const token = createToken(user);
+    res.cookie('jwt', token);
+    res.redirect('/');
+  } catch (err) {
+    const error = errorParser(err);
+    res.render('user/register', { email: userInput.email, error });
+  }
+});
+userController.get('/logout', (req, res) => {
+  res.clearCookie('jwt');
+  res.redirect('/');
+});
+
+export default userController;
