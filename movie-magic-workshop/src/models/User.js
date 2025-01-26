0cbcacabbfc9a01d5d9584e2cfc78451a8aa78d8
vladinSon <vladinson009@icloud.com>
2025-01-26 19:48:33 +0100

    added collation unique emails in user schema

--- movie-magic-workshop/src/models/User.js
+++ movie-magic-workshop/src/models/User.js
@@ -1,16 +1,23 @@
 import { Schema, model } from 'mongoose';
 import bcrypt from 'bcrypt';
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  // ! COLLATION IN SCHEMA LEVEL
+  { collation: { locale: 'en', strength: 2 } }
+);
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
     const hash = await bcrypt.hash(this.password, 11);
