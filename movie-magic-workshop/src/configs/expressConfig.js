fc66851faa2d422028147678e2cabef7ab022706
vladinSon <vladinson009@icloud.com>
2025-01-26 16:11:42 +0100

    set locals if valid cookie

--- movie-magic-workshop/src/configs/expressConfig.js
+++ movie-magic-workshop/src/configs/expressConfig.js
@@ -2,11 +2,13 @@ import express from 'express';
 import routes from '../routes.js';
 import pageTitle from '../middlewares/pageTitle.js';
 import cookieParser from 'cookie-parser';
+import cookieMiddleware from '../middlewares/cookieMiddleware.js';
 // * middlewares for following functionality
 export default function (app) {
   app.use(express.static('public')); // ! load static files
   app.use(express.urlencoded({ extended: false })); // ! req.body(POST) || req.query(GET)
   app.use(cookieParser()); // ! req.cookies
   app.use(pageTitle); // ! dynamic changes of page title
+  app.use(cookieMiddleware); // ! set res.locals.user if valid cookie
   app.use(routes); // ! main router for url
 }
