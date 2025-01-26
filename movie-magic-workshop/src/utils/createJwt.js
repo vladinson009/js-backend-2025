1776f91b5b682c473817a9119847d4d7eed94fe4
vladinSon <vladinson009@icloud.com>
2025-01-26 16:14:30 +0100

    create jwt custom function

--- movie-magic-workshop/src/utils/createJwt.js
+++ movie-magic-workshop/src/utils/createJwt.js
@@ -1,6 +1,10 @@
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT_SECRET_WORD } from '../constants.js';
 
-function createToken(payload) {
+export default function createToken(userData) {
+  const payload = {
+    email: userData.email,
+    _id: userData._id.toString(),
+  };
   return jsonwebtoken.sign(payload, JWT_SECRET_WORD, { expiresIn: '2d' });
 }
