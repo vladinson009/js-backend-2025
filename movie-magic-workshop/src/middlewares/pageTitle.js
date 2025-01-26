0a788652f8f1d2eeba935c40e559b913205d53ad
vladinSon <vladinson009@icloud.com>
2025-01-26 15:01:32 +0100

    added title for Login & Register & Edit Movie

--- movie-magic-workshop/src/middlewares/pageTitle.js
+++ movie-magic-workshop/src/middlewares/pageTitle.js
@@ -5,6 +5,8 @@ const titles = {
   '/movies/create': 'Create Movie',
   '/movies/search': 'Search Page',
   '/cast/create': 'Create Cast Page',
+  '/users/login': 'Login Page',
+  '/users/register': 'Register Page',
 };
 
 export default function pageTitle(req, res, next) {
@@ -13,6 +15,8 @@ export default function pageTitle(req, res, next) {
     res.locals.pageTitle = title;
   } else if (req.url.includes('/movies/details')) {
     res.locals.pageTitle = 'Movie Details';
+  } else if (req.url.includes('/movies/edit')) {
+    res.locals.pageTitle = 'Edit Movie';
   } else if (req.url.includes('/cast/attach/')) {
     res.locals.pageTitle = 'Attach Cast Page';
   }
