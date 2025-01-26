2acb931338f74094403cbd3d5aad32d6dceace5d
vladinSon <vladinson009@icloud.com>
2025-01-26 15:00:52 +0100

    added comments

--- movie-magic-workshop/src/configs/mongooseConfig.js
+++ movie-magic-workshop/src/configs/mongooseConfig.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { DB_URL } from '../constants.js';
+// * mongoose connection
 export default async function () {
   try {
     await mongoose.connect(DB_URL);
