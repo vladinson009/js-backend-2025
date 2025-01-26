09f7a70d53222b8d1fdd44642652e34b02b7549c
vladinSon <vladinson009@icloud.com>
2025-01-26 16:11:12 +0100

    added user route

--- movie-magic-workshop/src/routes.js
+++ movie-magic-workshop/src/routes.js
@@ -3,12 +3,13 @@ import homeController from './controllers/homeController.js';
 import staticController from './controllers/staticController.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castController.js';
+import userController from './controllers/userController.js';
 const routes = Router();
 // TODO: Create Routes protection
 routes.use(homeController); // * Home page
 routes.use(staticController); // * About page
 routes.use('/movies', movieController); // * Movie pages
 routes.use('/cast', castController); // * Cast pages
-
+routes.use('/users', userController); // * User Pages
 routes.use('*', (req, res) => res.redirect('/404'));
 export default routes;
