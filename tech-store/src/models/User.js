d7fa044be9744848d36a2e6777244d62367c31cf
vladinSon <vladinson009@icloud.com>
2025-02-01 22:52:44 +0100

    added collation for user schema

--- tech-store/src/models/User.js
+++ tech-store/src/models/User.js
@@ -1,19 +1,23 @@
 import { Schema, model } from 'mongoose';
 import bcrypt from 'bcrypt';
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { collation: { locale: 'en', strength: 2 } }
+);
 
 userSchema.pre('save', async function () {
   const hashedPassword = await bcrypt.hash(this.password, 11);
