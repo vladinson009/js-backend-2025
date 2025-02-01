a34546cdc2a9797aa4f6848f9fcde928cdde37a7
vladinSon <vladinson009@icloud.com>
2025-02-01 16:35:07 +0100

    added recipes & users routes

--- home-cooking-recipes/src/routes.js
+++ home-cooking-recipes/src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
+import recipeController from './controllers/recipeController.js';
+import userController from './controllers/userController.js';
 
 const routes = Router();
 routes.use(homeController);
+routes.use('/recipes', recipeController);
+routes.use('/users', userController);
 
 routes.use('*', (req, res) => res.render('404'));
 export default routes;
