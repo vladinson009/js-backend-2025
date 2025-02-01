c0f0760a0d61cc64bc89fe47eda70a7c94acfacf
vladinSon <vladinson009@icloud.com>
2025-02-01 17:32:58 +0100

    login & register post requests

--- home-cooking-recipes/src/controllers/userController.js
+++ home-cooking-recipes/src/controllers/userController.js
@@ -1,11 +1,34 @@
 import { Router } from 'express';
+import userService from '../services/userService.js';
 
 const userController = Router();
 
 userController.get('/login', (req, res) => {
   res.render('user/login');
 });
+userController.post('/login', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const token = await userService.login(userInput);
+    res.cookie('jwt', token);
+    res.redirect('/');
+  } catch (err) {
+    const error = err.message;
+    res.render('user/login', { userInput, error });
+  }
+});
 userController.get('/register', (req, res) => {
   res.render('user/register');
+});
+userController.post('/register', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const token = await userService.register(userInput);
+    res.cookie('jwt', token);
+    res.redirect('/');
+  } catch (err) {
+    const error = err.message;
+    res.render('user/register', { userInput, error });
+  }
 });
 export default userController;
