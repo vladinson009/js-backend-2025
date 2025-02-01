f28a5346c672668ad2ed733c3756c93af9723bb7
vladinSon <vladinson009@icloud.com>
2025-02-01 17:33:41 +0100

    user login and register services

--- home-cooking-recipes/src/services/userService.js
+++ home-cooking-recipes/src/services/userService.js
@@ -1,5 +1,55 @@
 import User from '../models/User.js';
-
-function register(data) {
-  User.create();
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import { JWT_SECRET } from '../constants.js';
+async function login(data) {
+  try {
+    for (const field in data) {
+      if (data[field].trim() == '') {
+        throw new Error(`${field} field is required!`);
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
+  try {
+    const user = await User.findOne({ email: data.email });
+    if (user == null) {
+      throw new Error('Email or password does not match');
+    }
+    const isMatchinPassword = await bcrypt.compare(data.password, user.password);
+    if (!isMatchinPassword) {
+      throw new Error('Email or password does not match');
+    }
+    const payload = {
+      username: user.username,
+      email: user.email,
+      _id: user._id,
+    };
+    return jsonwebtoken.sign(payload, JWT_SECRET, { expiresIn: '2h' });
+  } catch (error) {
+    throw error;
+  }
 }
+async function register(data) {
+  try {
+    for (const field in data) {
+      if (data[field].trim() == '') {
+        throw new Error(`${field} field is required!`);
+      }
+    }
+    if (data.password != data['re-password']) {
+      throw new Error('Password does not match!');
+    }
+    const user = await User.create(data);
+    const payload = {
+      username: user.username,
+      email: user.email,
+      _id: user._id,
+    };
+    return jsonwebtoken.sign(payload, JWT_SECRET, { expiresIn: '2h' });
+  } catch (err) {
+    throw err;
+  }
+}
+export default { login, register };
