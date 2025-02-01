1098af2f62caa36382a2b824aa226e8c2dbd0d38
vladinSon <vladinson009@icloud.com>
2025-02-01 22:51:35 +0100

    added user router

--- tech-store/src/routes.js
+++ tech-store/src/routes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import homeController from './controllers/homeController.js';
+import userController from './controllers/userController.js';
 
 const router = Router();
 
+router.use(homeController);
+router.use('/users', userController);
+
+router.use('*', (req, res) => res.render('404'));
 export default router;
