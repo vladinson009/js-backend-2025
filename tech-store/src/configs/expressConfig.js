19e28be363b7794118cda2ecc7b347f46c3c3fbc
vladinSon <vladinson009@icloud.com>
2025-02-01 22:52:04 +0100

    static files and user information

--- tech-store/src/configs/expressConfig.js
+++ tech-store/src/configs/expressConfig.js
@@ -2,11 +2,13 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import router from '../routes.js';
 import pageTitle from '../middlewares/pageTitle.js';
+import isUser from '../middlewares/isUser.js';
 
 export default function (app) {
-  //!TODO: app.use('/static', express.static('public'));
+  app.use('/styles', express.static('public'));
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(pageTitle);
+  app.use(isUser);
   app.use(router);
 }
