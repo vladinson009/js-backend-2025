de2f279362b64035a61dbba6bcc57634c65de075
vladinSon <vladinson009@icloud.com>
2025-02-01 21:40:46 +0100

    hbs configs

---
+++ tech-store/src/configs/hbsConfig.js
@@ -0,0 +1,7 @@
+import handlebars from 'express-handlebars';
+
+export default function (app) {
+  app.set('view engine', 'hbs');
+  app.set('views', 'src/views');
+  app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+}
