3c32a5cc6578bf6f8ce12cca3ef0e4f86c554b7d
vladinSon <vladinson009@icloud.com>
2025-02-01 16:23:22 +0100

    added urlencoded & cookie parser & dynamic page title

--- home-cooking-recipes/src/configs/expressConfig.js
+++ home-cooking-recipes/src/configs/expressConfig.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import routes from '../routes.js';
+import pageTitle from '../middlewares/pageTitle.js';
+import cookieParser from 'cookie-parser';
 
 export default function expressConfig(app) {
   app.use('/styles', express.static('public'));
-
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(pageTitle);
   app.use(routes);
 }
