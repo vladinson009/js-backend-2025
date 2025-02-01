d672f0bab713f8162dba675d97a48867d2ddc910
vladinSon <vladinson009@icloud.com>
2025-02-01 16:35:28 +0100

    added user controller get requests

---
+++ home-cooking-recipes/src/controllers/userController.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const userController = Router();
+
+userController.get('/login', (req, res) => {
+  res.render('user/login');
+});
+userController.get('/register', (req, res) => {
+  res.render('user/register');
+});
+export default userController;
