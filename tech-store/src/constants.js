6fb39a35b1dc481a3164995eba95127749d23157
vladinSon <vladinson009@icloud.com>
2025-02-01 22:51:41 +0100

    secret token added

--- tech-store/src/constants.js
+++ tech-store/src/constants.js
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || 3000;
 export const CONNECTION_STRING =
   process.env.CONNECTION_STRING || 'mongodb://localhost:27017/tech-store-jan-25';
+export const SECRET_TOKEN = process.env.SECRET_TOKEN || null;
