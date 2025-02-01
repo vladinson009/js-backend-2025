2028da98c0d1cede8b134db2496fb089d4cf26f6
vladinSon <vladinson009@icloud.com>
2025-02-01 17:33:11 +0100

    hash password before registering new user

--- home-cooking-recipes/src/models/User.js
+++ home-cooking-recipes/src/models/User.js
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-
+import bcrypt from 'bcrypt';
 const userSchema = new Schema(
   {
     username: {
@@ -17,5 +17,12 @@ const userSchema = new Schema(
   },
   { collation: { locale: 'en', strength: 2 } }
 );
-
+userSchema.pre('save', async function () {
+  try {
+    const hash = await bcrypt.hash(this.password, 11);
+    this.password = hash;
+  } catch (error) {
+    throw error;
+  }
+});
 export default model('user', userSchema);
