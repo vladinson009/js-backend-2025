854a688cfb366a6488577f08fdfe06ff8b55122c
vladinSon <vladinson009@icloud.com>
2025-02-01 17:32:33 +0100

    middleware for navigation

--- home-cooking-recipes/src/configs/expressConfig.js
+++ home-cooking-recipes/src/configs/expressConfig.js
@@ -2,11 +2,13 @@ import express from 'express';
 import routes from '../routes.js';
 import pageTitle from '../middlewares/pageTitle.js';
 import cookieParser from 'cookie-parser';
+import isUser from '../middlewares/isUser.js';
 
 export default function expressConfig(app) {
   app.use('/styles', express.static('public'));
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(pageTitle);
+  app.use(isUser);
   app.use(routes);
 }
