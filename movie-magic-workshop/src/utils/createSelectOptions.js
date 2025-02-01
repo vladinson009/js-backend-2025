eadd1d9851b7f8d02835343321dd06e21f83e864
vladinSon <vladinson009@icloud.com>
2025-02-01 14:58:14 +0100

    #

--- movie-magic-workshop/src/utils/createSelectOptions.js
+++ movie-magic-workshop/src/utils/createSelectOptions.js
@@ -1,6 +1,7 @@
 // Create dynamic select options for game platforms
 export default function createSelectOptions(object, selected) {
   const tuples = Object.entries(object);
+
   return tuples.map(([value, content]) => {
     if (value == selected) {
       return { value, content, selected: true };
