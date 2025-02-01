c321a5fdd2a753b3be1a693369d92358d3b757ec
vladinSon <vladinson009@icloud.com>
2025-02-01 21:57:54 +0100

    added page title middleware to express

--- tech-store/src/configs/expressConfig.js
+++ tech-store/src/configs/expressConfig.js
@@ -1,10 +1,12 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import router from '../routes.js';
+import pageTitle from '../middlewares/pageTitle.js';
 
 export default function (app) {
   //!TODO: app.use('/static', express.static('public'));
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(pageTitle);
   app.use(router);
 }
