7f3fccaae3a01f4c9bdf7c2b47b7eefed6da1504
vladinSon <vladinson009@icloud.com>
2025-02-01 23:41:22 +0100

    add devices routes

--- tech-store/src/routes.js
+++ tech-store/src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import userController from './controllers/userController.js';
+import deviceController from './controllers/deviceController.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/users', userController);
+router.use('/devices', deviceController);
 
 router.use('*', (req, res) => res.render('404'));
 export default router;
