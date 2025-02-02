baa206fc3587ad62c31d67e860e53bd18324b363
vladinSon <vladinson009@icloud.com>
2025-02-02 01:08:09 +0100

    sanitize input & implemented preferDevice, deleteDeviceById, updateById,getAllByUserId,getPreferedDevicesByUserId

--- tech-store/src/services/deviceService.js
+++ tech-store/src/services/deviceService.js
@@ -1,19 +1,17 @@
 import Device from '../models/Device.js';
+import sanitizeInput from '../utils/sanitizeInput.js';
 // ! CREATE NEW DEVICE
 function createDevice(userInput, owner) {
-  for (const field in userInput) {
-    if (userInput[field].trim() == '') {
-      throw new Error(`${field} is required!`);
-    }
-  }
-  if (Number.isNaN(Number(userInput.price))) {
-    throw new Error('Price must be a number!');
-  }
+  sanitizeInput(userInput);
   return Device.create({ ...userInput, owner });
 }
 // ! GET ALL DEVICES
 function getAllDevices() {
   return Device.find({});
+}
+// ! GET ALL DEVICES BY USER ID
+function getAllByUserId(userId) {
+  return Device.find({ owner: userId });
 }
 // ! GET LAST THREE DEVICES
 function getLastThreeDevices() {
@@ -23,10 +21,37 @@ function getLastThreeDevices() {
 function getDeviceById(deviceId) {
   return Device.findById(deviceId);
 }
-
+// ! UPDATE DEVICE BY ID
+function updateDeviceById(deviceId, userInput) {
+  sanitizeInput(userInput);
+  return Device.findByIdAndUpdate(deviceId, userInput, { runValidators: true });
+}
+// ! DELETE DEVICE BY ID
+function deleteDeviceById(deviceId) {
+  return Device.findByIdAndDelete(deviceId);
+}
+// ! PREFER DEVICE
+function preferDevice(deviceId, userId) {
+  return Device.findByIdAndUpdate(deviceId, {
+    $addToSet: { prefferedList: userId },
+  });
+}
+// ! GET PREFERED DEVICE BY USERID
+function getPreferedDevicesByUserId(userId) {
+  return Device.find({
+    prefferedList: {
+      $in: [userId],
+    },
+  });
+}
 export default {
   createDevice,
   getLastThreeDevices,
   getDeviceById,
   getAllDevices,
+  preferDevice,
+  deleteDeviceById,
+  updateDeviceById,
+  getAllByUserId,
+  getPreferedDevicesByUserId,
 };
