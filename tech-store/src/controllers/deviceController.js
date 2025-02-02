5828622721847e80c462778392eed5c552121ee6
vladinSon <vladinson009@icloud.com>
2025-02-02 01:07:10 +0100

    implemented more device controller

--- tech-store/src/controllers/deviceController.js
+++ tech-store/src/controllers/deviceController.js
@@ -33,9 +33,95 @@ deviceController.get('/details/:deviceId', async (req, res) => {
     const device = await deviceService.getDeviceById(deviceId).lean();
     const owner = device.owner.toString();
     const currentUserId = res.locals?.user?._id;
+    const isPreffered = device.prefferedList.some((el) => el.toString() == currentUserId);
 
     const isOwner = owner == currentUserId;
-    res.render('device/details', { device, isOwner });
+    res.render('device/details', { device, isOwner, isPreffered });
+  } catch (error) {
+    res.redirect('/404');
+  }
+});
+// ! EDIT DEVICE
+deviceController.get('/edit/:deviceId', loggedOnly, async (req, res) => {
+  const { deviceId } = req.params;
+  const currentUserId = res.locals.user._id;
+  try {
+    const device = await deviceService.getDeviceById(deviceId).lean();
+    if (currentUserId != device.owner.toString()) {
+      return res.redirect('/404');
+    }
+    res.render('device/edit', { device });
+  } catch (error) {
+    res.redirect('/404');
+  }
+});
+deviceController.post('/edit/:deviceId', loggedOnly, async (req, res) => {
+  const { deviceId } = req.params;
+  const userInput = req.body;
+  try {
+    const device = await deviceService.getDeviceById(deviceId).lean();
+    const currentUserId = res.locals?.user._id;
+    if (currentUserId != device.owner.toString()) {
+      return res.redirect('/404');
+    }
+  } catch (error) {
+    return res.redirect('/404');
+  }
+  try {
+    await deviceService.updateDeviceById(deviceId, userInput);
+    res.redirect(`/devices/details/${deviceId}`);
+  } catch (err) {
+    res.render('device/edit', { error: err.message, device: userInput });
+  }
+});
+// ! DELETE DEVICE
+deviceController.get('/delete/:deviceId', loggedOnly, async (req, res) => {
+  const { deviceId } = req.params;
+  try {
+    const device = await deviceService.getDeviceById(deviceId).lean();
+    const currentUserId = res.locals.user._id;
+    const ownerId = device.owner.toString();
+    if (currentUserId != ownerId) {
+      return res.redirect('/404');
+    }
+    try {
+      await deviceService.deleteDeviceById(deviceId);
+      res.redirect('/devices/catalog');
+    } catch (err) {
+      res.render('device/details', { error: err.message, device });
+    }
+  } catch (error) {
+    res.redirect('/404');
+  }
+});
+// ! PREFER DEVICE
+deviceController.get('/prefer/:deviceId', loggedOnly, async (req, res) => {
+  const { deviceId } = req.params;
+  const currentUserId = res.locals.user._id;
+  try {
+    const device = await deviceService.getDeviceById(deviceId).lean();
+    if (device.owner.toString() == currentUserId) {
+      return res.redirect('/404');
+    }
+  } catch (error) {
+    return res.redirect('/404');
+  }
+  try {
+    await deviceService.preferDevice(deviceId, currentUserId);
+    res.redirect(`/devices/details/${deviceId}`);
+  } catch (error) {
+    res.redirect('/404');
+  }
+});
+// ! DEVICES PROFILE
+deviceController.get('/profile', loggedOnly, async (req, res) => {
+  const userId = res.locals?.user._id;
+  try {
+    const [devices, preferedDevices] = await Promise.all([
+      deviceService.getAllByUserId(userId).lean(),
+      deviceService.getPreferedDevicesByUserId(userId).lean(),
+    ]);
+    res.render('device/profile', { devices, preferedDevices });
   } catch (error) {
     res.redirect('/404');
   }
