4ec8543cb249264213541a78f53695d5229d632d
vladinSon <vladinson009@icloud.com>
2025-02-09 01:48:54 +0100

    added cookie middleware

--- book-talk/src/configs/expressConfig.js
+++ book-talk/src/configs/expressConfig.js
@@ -1,10 +1,12 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import router from '../router.js';
+import cookieMiddleware from '../middlewares/cookieMiddleware.js';
 
 export default function (app) {
   app.use(express.static('public'));
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(cookieMiddleware);
   app.use(router);
 }
