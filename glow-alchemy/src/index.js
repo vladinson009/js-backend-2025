1dd1a1b0b7a484dea18547bf654643a63be02815
vladinSon <vladinson009@icloud.com>
2025-02-09 14:08:07 +0100

    load configs from hbs and express

--- glow-alchemy/src/index.js
+++ glow-alchemy/src/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import { PORT } from './constants.js';
+import hbsConfig from './configs/hbsConfig.js';
+import expressConfig from './configs/expressConfig.js';
 
 const app = express();
 
+// ? configs
+expressConfig(app);
+hbsConfig(app);
+
+app.get('/', (req, res) => res.render('home'));
 app.listen(PORT, () =>
   console.log('Server is listening on http://localhost:3000...')
 );
