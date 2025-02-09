3d0cac98eb0c421651275179f072647737f1539a
vladinSon <vladinson009@icloud.com>
2025-02-09 15:21:00 +0100

    mongoDB connection url and jwt secret(practicing purpose)

--- glow-alchemy/src/constants.js
+++ glow-alchemy/src/constants.js
@@ -1,2 +1,4 @@
 export const PORT = 3000;
 export const SALT_ROUNDS = 11;
+export const CONNECTION_URL = 'mongodb://localhost:27017/glow-alchemy-jan-25';
+export const JWT_SECRET = '329laln2bdqkvvvp';
