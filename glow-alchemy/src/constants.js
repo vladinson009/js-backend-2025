4bc162a3652342a7fa278231ec462c7c520288bd
vladinSon <vladinson009@icloud.com>
2025-02-09 16:22:26 +0100

    auth cookie name

--- glow-alchemy/src/constants.js
+++ glow-alchemy/src/constants.js
@@ -2,3 +2,4 @@ export const PORT = 3000;
 export const SALT_ROUNDS = 11;
 export const CONNECTION_URL = 'mongodb://localhost:27017/glow-alchemy-jan-25';
 export const JWT_SECRET = '329laln2bdqkvvvp';
+export const AUTH_COOKIE_NAME = 'authCookie';
