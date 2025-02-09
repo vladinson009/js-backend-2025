8b32e864916b2500deb6de525ccc1ec83b060389
vladinSon <vladinson009@icloud.com>
2025-02-09 16:23:01 +0100

    validate cookie middleware

---
+++ glow-alchemy/src/middlewares/validateCookie.js
@@ -0,0 +1,17 @@
+import { AUTH_COOKIE_NAME, JWT_SECRET } from '../constants.js';
+import { verify } from '../utils/asyncJwt.js';
+
+export default async function (req, res, next) {
+  if (req.cookies[AUTH_COOKIE_NAME]) {
+    try {
+      const user = await verify(req.cookies[AUTH_COOKIE_NAME], JWT_SECRET);
+      res.locals.user = user;
+    } catch (error) {
+      res.clearCookie(AUTH_COOKIE_NAME);
+    } finally {
+      next();
+    }
+  } else {
+    next();
+  }
+}
