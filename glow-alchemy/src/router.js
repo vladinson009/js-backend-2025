c2fbccd2f4c45866154ccece9184e4b9e897b7dc
vladinSon <vladinson009@icloud.com>
2025-02-09 15:21:19 +0100

    connected home controller and user controller to the main route

--- glow-alchemy/src/router.js
+++ glow-alchemy/src/router.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import userController from './controllers/userController.js';
+import homeController from './controllers/homeController.js';
 
 const router = Router();
+router.use(homeController);
+router.use('/users', userController);
 
-// router.use('');
+router.use('/404', (req, res) => res.render('404'));
+router.use('*', (req, res) => res.redirect('/404'));
 
 export default router;
