76596d5c9903a493575d2ca166c6f8dd3a86a74a
vladinSon <vladinson009@icloud.com>
2025-02-09 15:46:05 +0100

    added products controller to the main route

--- glow-alchemy/src/router.js
+++ glow-alchemy/src/router.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import userController from './controllers/userController.js';
 import homeController from './controllers/homeController.js';
+import cosmeticController from './controllers/cosmeticController.js';
 
 const router = Router();
+
 router.use(homeController);
 router.use('/users', userController);
-
+router.use('/products', cosmeticController);
 router.use('/404', (req, res) => res.render('404'));
 router.use('*', (req, res) => res.redirect('/404'));
 
