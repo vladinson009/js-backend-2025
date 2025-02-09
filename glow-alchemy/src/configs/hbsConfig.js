7b8d99616b1b75f5d64bfc1b8e9102bbb09eb006
vladinSon <vladinson009@icloud.com>
2025-02-09 14:08:17 +0100

    config files

---
+++ glow-alchemy/src/configs/hbsConfig.js
@@ -0,0 +1,16 @@
+import handlebars from 'express-handlebars';
+
+export default function (app) {
+  app.set('view engine', 'hbs');
+  app.set('views', 'src/views');
+  app.engine(
+    'hbs',
+    handlebars.engine({
+      extname: 'hbs',
+      helpers: { setTitle },
+    })
+  );
+}
+function setTitle(value) {
+  this.pageTitle = value || 'Page title';
+}
