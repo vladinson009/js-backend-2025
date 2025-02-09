b4c82171b77a79549ce44bdea4dfe0c6d8b8c176
vladinSon <vladinson009@icloud.com>
2025-02-09 01:49:04 +0100

    userController

--- book-talk/src/controllers/userController.js
+++ book-talk/src/controllers/userController.js
@@ -1,13 +1,43 @@
 import { Router } from 'express';
+import userService from '../services/userService.js';
+import { AUTH_COOKIE_NAME } from '../constants.js';
+import errorParser from '../utils/parseError.js';
 
 const userController = Router();
 
 userController.get('/login', (req, res) => {
   res.render('user/login');
 });
+userController.post('/login', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const user = await userService.login(userInput);
+    res.cookie(AUTH_COOKIE_NAME, user);
+    res.redirect('/');
+  } catch (err) {
+    const error = errorParser(err);
+    res.render('user/register', { error, userInput });
+  }
+});
 
 userController.get('/register', (req, res) => {
   res.render('user/register');
+});
+userController.post('/register', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const user = await userService.register(userInput);
+    res.cookie(AUTH_COOKIE_NAME, user);
+    res.redirect('/');
+  } catch (err) {
+    const error = errorParser(err);
+    res.render('user/register', { error, userInput });
+  }
+});
+
+userController.get('/logout', (req, res) => {
+  res.clearCookie(AUTH_COOKIE_NAME);
+  res.redirect('/');
 });
 
 export default userController;
