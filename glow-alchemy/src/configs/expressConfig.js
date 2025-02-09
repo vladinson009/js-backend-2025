2d3a6bde65ba3310ee69c705e821bd802648edb6
vladinSon <vladinson009@icloud.com>
2025-02-09 14:33:13 +0100

    added url encoded and router middleware

--- glow-alchemy/src/configs/expressConfig.js
+++ glow-alchemy/src/configs/expressConfig.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import router from '../router.js';
 
 export default function (app) {
   app.use(express.static('public'));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(router);
 }
