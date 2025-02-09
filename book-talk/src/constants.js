c8eff1cec0762e34bb8dc90b10d676958876288d
vladinSon <vladinson009@icloud.com>
2025-02-09 01:48:47 +0100

    new constants for demo purposes

--- book-talk/src/constants.js
+++ book-talk/src/constants.js
@@ -1,2 +1,4 @@
 export const PORT = 3000;
 export const CONNECTION_STRING = 'mongodb://localhost:27017/book-talk-jan-25';
+export const JWT_SECRET = '9823dhlkqwl';
+export const AUTH_COOKIE_NAME = 'authCookie';
