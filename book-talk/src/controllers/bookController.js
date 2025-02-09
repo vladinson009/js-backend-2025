17b3c2fc3c28fa7aab168e00f8a1a3c85c12ff0a
vladinSon <vladinson009@icloud.com>
2025-02-09 02:09:46 +0100

    get catalog and details view

--- book-talk/src/controllers/bookController.js
+++ book-talk/src/controllers/bookController.js
@@ -4,23 +4,39 @@ import errorParser from '../utils/parseError.js';
 
 const bookController = Router();
 
-bookController.get('/catalog', (req, res) => {
-  res.render('book/catalog');
+bookController.get('/catalog', async (req, res) => {
+  try {
+    const books = await bookService.getAll().lean();
+    res.render('book/catalog', { books });
+  } catch (error) {
+    res.redirect('/404');
+  }
 });
 bookController.get('/create', (req, res) => {
   res.render('book/create');
 });
-bookController.post('create', async (req, res) => {
+bookController.post('/create', async (req, res) => {
   const userInput = req.body;
-  const userId = res.locals?._id;
+  const userId = res.locals?.user._id;
   if (userId) {
     try {
-      await bookService.create(userInput);
+      await bookService.create(userInput, userId);
       res.redirect('/');
     } catch (err) {
+      console.log(err.message);
+
       const error = errorParser(err);
       res.render('book/create', error, userInput);
     }
+  }
+});
+bookController.get('/details/:bookId', async (req, res) => {
+  const { bookId } = req.params;
+  try {
+    const book = await bookService.getById(bookId).lean();
+    res.render('book/details', { book });
+  } catch (error) {
+    res.redirect('/404');
   }
 });
 export default bookController;
