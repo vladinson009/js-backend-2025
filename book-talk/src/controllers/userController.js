75aea4a79534ec2e08b64e77a00e06164623cc69
vladinSon <vladinson009@icloud.com>
2025-02-09 03:24:52 +0100

    route gurads

--- book-talk/src/controllers/userController.js
+++ book-talk/src/controllers/userController.js
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import userService from '../services/userService.js';
 import { AUTH_COOKIE_NAME } from '../constants.js';
 import errorParser from '../utils/parseError.js';
+import { guestsOnly, loggedOnly } from '../middlewares/routeGuards.js';
 
 const userController = Router();
 
-userController.get('/login', (req, res) => {
+userController.get('/login', guestsOnly, (req, res) => {
   res.render('user/login');
 });
-userController.post('/login', async (req, res) => {
+userController.post('/login', guestsOnly, async (req, res) => {
   const userInput = req.body;
   try {
     const user = await userService.login(userInput);
@@ -16,14 +17,14 @@ userController.post('/login', async (req, res) => {
     res.redirect('/');
   } catch (err) {
     const error = errorParser(err);
-    res.render('user/register', { error, userInput });
+    res.render('user/login', { error, userInput });
   }
 });
 
-userController.get('/register', (req, res) => {
+userController.get('/register', guestsOnly, (req, res) => {
   res.render('user/register');
 });
-userController.post('/register', async (req, res) => {
+userController.post('/register', guestsOnly, async (req, res) => {
   const userInput = req.body;
   try {
     const user = await userService.register(userInput);
@@ -35,7 +36,7 @@ userController.post('/register', async (req, res) => {
   }
 });
 
-userController.get('/logout', (req, res) => {
+userController.get('/logout', loggedOnly, (req, res) => {
   res.clearCookie(AUTH_COOKIE_NAME);
   res.redirect('/');
 });
