3a94e059e091c93c8dcf8293ab67531cba3f9a19
vladinSon <vladinson009@icloud.com>
2025-02-09 16:22:39 +0100

    added cookie parser and validate cookie

--- glow-alchemy/src/configs/expressConfig.js
+++ glow-alchemy/src/configs/expressConfig.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import router from '../router.js';
-
+import cookieParser from 'cookie-parser';
+import validateCookie from '../middlewares/validateCookie.js';
 export default function (app) {
   app.use(express.static('public'));
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(validateCookie);
   app.use(router);
 }
