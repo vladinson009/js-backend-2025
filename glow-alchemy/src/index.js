f3c58e69880c31c66c3b5afeb8c28d802672a31a
vladinSon <vladinson009@icloud.com>
2025-02-09 15:21:30 +0100

    mongoose config

--- glow-alchemy/src/index.js
+++ glow-alchemy/src/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import { PORT } from './constants.js';
 import hbsConfig from './configs/hbsConfig.js';
 import expressConfig from './configs/expressConfig.js';
+import mongooseConfig from './configs/mongooseConfig.js';
+import { PORT } from './constants.js';
 
 const app = express();
 
 // ? configs
 expressConfig(app);
 hbsConfig(app);
-
-app.get('/', (req, res) => res.render('home'));
+try {
+  await mongooseConfig();
+  console.log('DB Connected successfully!...');
+} catch (error) {
+  console.log('DB Can not connect!: ' + error.message);
+}
 app.listen(PORT, () =>
   console.log('Server is listening on http://localhost:3000...')
 );
