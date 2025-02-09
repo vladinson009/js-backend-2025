634486e4d93aaabb5c53d04cba62c6add877c199
vladinSon <vladinson009@icloud.com>
2025-02-09 16:22:50 +0100

    post login and post register requests

--- glow-alchemy/src/controllers/userController.js
+++ glow-alchemy/src/controllers/userController.js
@@ -1,12 +1,39 @@
 import { Router } from 'express';
 import userService from '../services/userService.js';
+import { AUTH_COOKIE_NAME } from '../constants.js';
+import errorParser from '../utils/errorParser.js';
 
 const userController = Router();
 
 userController.get('/login', async (req, res) => {
   res.render('user/login');
 });
+userController.post('/login', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const token = await userService.login(userInput);
+    res.cookie(AUTH_COOKIE_NAME, token, {
+      maxAge: 60 * 60 * 1000,
+      httpOnly: true,
+    });
+    res.redirect('/');
+  } catch (err) {
+    const error = errorParser(err);
+    res.render('user/login', { error, userInput });
+  }
+});
 userController.get('/register', (req, res) => {
   res.render('user/register');
+});
+userController.post('/register', async (req, res) => {
+  const userInput = req.body;
+  try {
+    const token = await userService.register(userInput);
+    res.cookie(AUTH_COOKIE_NAME, token, { maxAge: 60 * 60 * 1000, httpOnly: true });
+    res.redirect('/');
+  } catch (err) {
+    const error = errorParser(err);
+    res.render('user/register', { error, userInput });
+  }
 });
 export default userController;
