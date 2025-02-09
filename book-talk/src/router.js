5a6234e99c0715bbb3272dda193dbfb10f11688a
vladinSon <vladinson009@icloud.com>
2025-02-09 01:48:34 +0100

    added routes

--- book-talk/src/router.js
+++ book-talk/src/router.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import userController from './controllers/userController.js';
+import bookController from './controllers/bookController.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/users', userController);
+router.use('/books', bookController);
 router.use('*', (req, res) => res.render('404'));
 
 export default router;
